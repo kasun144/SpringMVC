dcee3e4d8d1965c2d4ab81eb27d2497d7a7e016c
kasun144 <kasun144@users.noreply.github.com>
2021-03-02 23:15:32 +0530

    Initial commit

---
+++ src/main/java/net/codejava/ProductService.java
@@ -0,0 +1,34 @@
+package net.codejava;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll(String keyword) {
+        if (keyword != null) {
+            return repo.search(keyword);
+        }
+        return repo.findAll();
+    }
+	
+	public void save(Product product) {
+		repo.save(product);
+	}
+	
+	public Product get(long id) {
+		return repo.findById(id).get();
+	}
+	
+	public void delete(long id) {
+		repo.deleteById(id);
+	}
+}
